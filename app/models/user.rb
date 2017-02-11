744da5f01c89b8e6c577be3d9877353935eb1d78
hiroki <hata@whatzmoney.com>
2017-02-11 17:46:55 +0900

    firstcommit

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+end
